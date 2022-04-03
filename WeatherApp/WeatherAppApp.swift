0d3f9a4359e4b38d118e8929b8cd832d3595dc00
LeeHsss <lii0730@icloud.com>
2022-04-03 13:22:48 +0900

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by LeeHsss on 2022/04/03.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
